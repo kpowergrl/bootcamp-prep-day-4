311bcd6cf588dbfc41e4689d41573a81df130f0c
Kelly Power <kpowergrl@gmail.com>
2019-01-17 19:45:56 -0800

    make greeting

--- main.js
+++ main.js
@@ -2,12 +2,20 @@
 function greet() {
     
     // get the inputs by id
+    var firstNameInput = document.getElementById('first-name');
+    var lastNameInput = document.getElementById('last-name');
     
 
     //get the values out of inputs
+    var firstName = firstNameInput.value;
+    var lastName = lastNameInput.value;
+
+    console.log(firstName, lastName);
 
 
     //make greeting
+    var greeting = 'Hello ' + firstName + ' ' + lastName + '!'
+    console.log(greeting);
 
 
     //display greeting
